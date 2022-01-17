bec0f770ca5c97b5dc13f01269141477f7bba987
Junus Hirner <hirner.family@web.de>
2022-01-17 11:50:23 +0100

    removed correlation part

--- helper.py
+++ helper.py
@@ -6,22 +6,11 @@ plt.ion()
 def plot(scores, mean_scores, correlationFood, correlationDanger):
     plt.clf()
 
-    plt.subplot(2,1,1)
     plt.title('Scores')
     plt.ylabel('Score')
     plt.plot(scores)
     plt.plot(mean_scores)
     plt.ylim(ymin=0)
-    plt.xlim(xmin=0)
-    plt.grid()
-
-    plt.subplot(2,1,2)
-    plt.title('Correlations')
-    plt.xlabel('Number of Games')
-    plt.ylabel('Correlation Coefficient')
-    plt.plot(correlationFood, color='c')
-    plt.plot(correlationDanger, color='m')
-    plt.ylim(ymin=-1, ymax=1)
     plt.xlim(xmin=0)
     plt.grid()
 
